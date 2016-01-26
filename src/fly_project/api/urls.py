41df5a2f40acb7f2229c8f6537858a879c81ba11
Bartlomiej Mika <bmika@icloud.com>
2016-01-26 16:04:22 -0500

    Latest development effort for the landpage. #18

--- src/fly_project/api/urls.py
+++ src/fly_project/api/urls.py
@@ -13,7 +13,7 @@ router = DefaultRouter()
 # Additionally, we include the login URLs for the browsable API.
 urlpatterns = [
     url(r'^api/', include(router.urls, namespace='api')),
-    url(r'^', include(router.urls)),
+#    url(r'^', include(router.urls)),
 #    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
     url(r'^api/login/', include('rest_social_auth.urls_token')),
 ]
