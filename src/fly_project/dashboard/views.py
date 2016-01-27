71ce504c721623fcd7a0d30e67fffeafbee332e8
Bartlomiej Mika <bmika@icloud.com>
2016-01-27 12:12:43 -0500

    Latest progress.

--- src/fly_project/dashboard/views.py
+++ src/fly_project/dashboard/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
 
-# Create your views here.
+
+def dashboard_page(request):
+    return render(request, 'dashboard/view.html',{})
