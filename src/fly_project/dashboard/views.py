f35b2c56c144c49e7511c2449824f1d4a32ce9a2
Bartlomiej Mika <bmika@icloud.com>
2016-01-27 15:16:22 -0500

    Integarted settings into templates

--- src/fly_project/dashboard/views.py
+++ src/fly_project/dashboard/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from fly_project import settings
 
 
 @login_required(login_url='/authentication')
 def dashboard_page(request):
-    return render(request, 'dashboard/view.html',{})
+    return render(request, 'dashboard/view.html',{
+        'settings': settings,
+    })
--- src/fly_project/landpage/views.py
+++ src/fly_project/landpage/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
+from fly_project import settings
 
 
 def land_page(request):
-    return render(request, 'landpage/view.html',{})
+    return render(request, 'landpage/view.html',{
+        'settings': settings,
+    })
