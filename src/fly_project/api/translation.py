0ebb4d05463f46155600286a02134f6edf9473df
Bartlomiej Mika <bmika@icloud.com>
2016-01-30 10:35:21 -0500

    Added comments to the translation. #56

--- src/fly_project/api/translation.py
+++ src/fly_project/api/translation.py
@@ -5,6 +5,15 @@ from api.models import Badge
 from api.models import Course
 from api.models import Quiz
 from api.models import Question
+"""
+    Translation.py (Required for "django-modeltranslation" third party library)
+    This file is used to apply multi-linguagl support for the particular
+    models and fields.
+"""
+
+
+# Register Objects for Translation
+#
 
 
 @register(Badge)
@@ -27,6 +36,8 @@ class QuestionTranslationOptions(TranslationOptions):
     fields = ('title', 'description', 'a', 'b', 'c', 'd', 'e', 'f', 'true_choice', 'false_choice',)
 
 
+# Register Objects to have Translations when in Admin
+#
 
 
 class BadgeAdmin(TranslationAdmin):
