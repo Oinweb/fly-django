de40dce7450d4c8fa1f2d8db7706c3fdcdd5b93a
Bartlomiej Mika <bmika@icloud.com>
2016-02-02 12:53:17 -0500

    Finished a friendly reminder when Goal was not accomplished.  #61

--- src/fly_project/mygoals/urls.py
+++ src/fly_project/mygoals/urls.py
@@ -7,5 +7,6 @@ urlpatterns = patterns('',
     url(r'^mygoals/credit$', views.credit_goals_page, name='credit_goals'),
     url(r'^mygoals/final$', views.final_goal_page, name='final_goals'),
     url(r'^mygoals/(\d+)/(\d+)/complete$', views.goal_complete_page, name='goal_completed'),
-                       
+    url(r'^mygoals/(\d+)/(\d+)/sorry$', views.goal_failed_page, name='goal_failed'),
+                   
 )
--- src/fly_project/mygoals/views.py
+++ src/fly_project/mygoals/views.py
@@ -155,3 +155,26 @@ def goal_complete_page(request, goal_type, goal_id):
         'goal_type': int(goal_type),
     })
 
+
+@login_required(login_url='/authentication')
+def goal_failed_page(request, goal_type, goal_id):
+    goal = None
+    try:
+        if goal_type == constants.SAVINGS_MYGOAL_TYPE:
+            goal = SavingsGoal.objects.get(id=goal_id)
+        elif goal_type == constants.CREDIT_MYGOAL_TYPE:
+            goal = CreditGoal.objects.get(id=goal_id)
+        elif goal_type == constants.GOAL_MYGOAL_TYPE:
+            goal = FinalGoal.objects.get(id=goal_id)
+    except Exception as e:
+        pass
+
+    final_goal = FinalGoal.objects.get_latest(request.user.id)
+    return render(request, 'mygoals/failed/view.html',{
+        'settings': settings,
+        'constants': constants,
+        'goal_id': int(goal_id),
+        'goal_type': int(goal_type),
+    })
+
+
