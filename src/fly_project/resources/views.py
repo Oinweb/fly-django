e7d84bde3a4f781104cb6f61f810c86a64a92e42
Bartlomiej Mika <bmika@icloud.com>
2016-02-23 13:39:04 -0500

    Migrating from custom ETL to using Django fixtures. #71

--- src/fly_project/resources/views.py
+++ src/fly_project/resources/views.py
@@ -1,14 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from fly_project import settings
 from fly_project import constants
 from api.models import ResourceLink
+from api.models import Badge, Me, Notification
 
 
 @login_required(login_url='/authentication')
 def resources_page(request):
+    # BADGE ID #28
+    # Check to see if the logged in User has the Badge wit the ID #1. If not
+    # then create it now.
+    me = get_object_or_404(Me, user=request.user.id)
+    badge = get_object_or_404(Badge, id=28)
+    if badge not in me.badges.all():
+        me.badges.add(badge)
+        Notification.objects.create(
+            type=2,
+            title=badge.title,
+            description=badge.description,
+            user=me.user,
+            badge=badge,
+        )
+    
     return render(request, 'resources/view.html',{
         'settings': settings,
         'constants': constants,
