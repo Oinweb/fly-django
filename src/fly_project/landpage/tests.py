4278eb0cfcbedc2db8dade93a02594b6d494f272
Bartlomiej Mika <bmika@icloud.com>
2016-02-24 10:24:09 -0500

    Added unit-tests for the land page.

--- src/fly_project/landpage/tests.py
+++ src/fly_project/landpage/tests.py
@@ -1,3 +1,31 @@
+import json
+from django.core.urlresolvers import resolve
+from django.http import HttpRequest
+from django.http import QueryDict
 from django.test import TestCase
+from django.test import Client
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from landpage import views
 
-# Create your tests here.
+
+class LandpageTest(TestCase):
+    def tearDown(self):
+        pass
+
+    def setUp(self):
+        pass
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/en/')
+        self.assertEqual(found.func,views.land_page)
+
+    def test_home_page_returns_correct_html(self):
+        parameters = {}
+        client = Client()
+        response = client.post(
+            '/en/',
+            data=parameters,
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Generic Login',response.content)
